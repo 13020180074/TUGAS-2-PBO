8cf9fe65641d95f2ee8d590828fbbe95b710f999
13020180074 <62000267+13020180074@users.noreply.github.com>
2020-03-16 22:35:57 +0800

    Add files via upload

---
+++ tugas2/tugas2.java
@@ -0,0 +1,23 @@
+/* Nama : Heryanti
+   STB	: 13020180074
+   KELAS : B1 TI
+   HARI / TGL : SENIN, 25 MARET 2020
+   WAKTU : 21.43
+*/
+
+
+public class tugas2{
+	public static void main(String[] args){
+			int detik, menit, jam, sisa1,sisa2;
+			detik = 5200;
+	
+		jam = detik/3600;
+		sisa1 = detik%3600;
+		
+		menit = (int) sisa1/60;
+		sisa2 = sisa1%60;
+		
+		System.out.println("Hasil Konversi : " + detik + " Detik = " + jam + " Jam : " + menit + " Menit : " + sisa2 + " Detik");
+	
+	}
+}
---
+++ tugas2/yanti.java
@@ -0,0 +1,23 @@
+/* NAMA  : Heryanti
+   STB	 : 13020180074
+   KELAS : B1 TI
+   HARI/TGL : SENIN, 16 MARET 2020
+   WAKTU : 21.30 
+*/
+
+
+public class yanti{
+	
+	public static void main(String [] args) {
+	String nim ="13020180074";
+	String nama ="Heryanti";
+	String jurusan ="Teknik Informatika";
+	String fakultas ="Ilmu Komputer";
+	System.out.println (" nim : " + nim);
+	System.out.println (" nama : " + nama);
+	System.out.println (" jurusan : " + jurusan);
+	System.out.println (" fakultas : " + fakultas);
+
+	}
+   }
+	
